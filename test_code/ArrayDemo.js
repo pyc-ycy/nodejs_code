d033934e7c35c131b901aebad55e1e8a47d7216f
ycy-ycy <2923616405@qq.com>
2020-03-30 22:34:37 +0800

    Update

--- test_code/ArrayDemo.js
+++ test_code/ArrayDemo.js
@@ -27,3 +27,29 @@ Array.prototype._sum = Array.prototype.sum ||
 Object.prototype.sum = Array.prototype._sum;
 var a = [1, 2, 3, 4, 5, 6, 7, 8, "9"];
 console.log(a.sum());
+//设计迭代器
+console.log("===================");
+console.log("设计迭代器");
+Array.prototype.each = function (f) {
+    try{
+        this.i || (this.i = 0);
+        if(this.length > 0 && f.constructor === Function){
+            while (this.i<this.length){
+                var e = this[this.i];
+                if(e && e.constructor === Array){
+                    e.each(f);
+                }else {
+                    f.apply(e, [e]);
+                }
+                this.i++;
+            }
+        }
+    }catch (w) {
+        return this;
+    }
+};
+var a1 = [1,[2, [3, 4]]];
+var f = function (x) {
+    console.log(x);
+};
+a1.each(f);
