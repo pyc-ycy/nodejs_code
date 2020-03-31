8df1bc52b6a03f4611f682d6c9923f7ce1ca2f7a
ycy-ycy <2923616405@qq.com>
2020-03-31 19:58:47 +0800

    Update

---
+++ test_code/abountFunction.js
@@ -0,0 +1,33 @@
+// It is called method when a function is set as a attribute of object
+// We can use Object.method to call a method.
+// Example:
+let obj = {
+    value : 0,
+    increment : function (inc) {
+        this.value += typeof inc === 'number' ? inc:1;
+    }
+};
+obj.increment();
+console.log(obj.value);      // 1
+obj.increment(2);
+console.log(obj.value);       // 3
+console.log("=======");
+// call() and apply() are the prototype method of Function,
+// they can regard goal function as a method and bind it on a goal object and then use it
+// example:
+function f(x, y){
+    return x+y;
+}
+console.log(f.call(null, 3, 4));
+console.log(f.apply(null, [3, 4]));
+function max() {
+    let m = Number.NEGATIVE_INFINITY;
+    for(let i=0; i<arguments.length; i++){
+        if(arguments[i] > m)
+            m = arguments[i];
+    }
+    return m;
+}
+let a = [23, 45, 2, 46, 62, 45, 56, 63];
+let m = max.apply(Object, a);
+console.log(m);
