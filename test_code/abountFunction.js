818018cfc03c593c8e8a88856d11e244985e9d3d
7wc98#14 <2923616405@qq.com>
2020-04-01 21:14:21 +0800

    Update

--- test_code/abountFunction.js
+++ test_code/abountFunction.js
@@ -2,9 +2,9 @@
 // We can use Object.method to call a method.
 // Example:
 let obj = {
-    value : 0,
-    increment : function (inc) {
-        this.value += typeof inc === 'number' ? inc:1;
+    value: 0,
+    increment: function (inc) {
+        this.value += typeof inc === 'number' ? inc : 1;
     }
 };
 obj.increment();
@@ -15,19 +15,34 @@ console.log("=======");
 // call() and apply() are the prototype method of Function,
 // they can regard goal function as a method and bind it on a goal object and then use it
 // example:
-function f(x, y){
-    return x+y;
+function f(x, y) {
+    return x + y;
 }
+
 console.log(f.call(null, 3, 4));
 console.log(f.apply(null, [3, 4]));
+
 function max() {
     let m = Number.NEGATIVE_INFINITY;
-    for(let i=0; i<arguments.length; i++){
-        if(arguments[i] > m)
+    for (let i = 0; i < arguments.length; i++) {
+        if (arguments[i] > m)
             m = arguments[i];
     }
     return m;
 }
+
 let a = [23, 45, 2, 46, 62, 45, 56, 63];
 let m = max.apply(Object, a);
 console.log(m);
+console.log("===========================");
+// Using "new" command can instantiate object, and the function of object will be activated and run during the process of new object
+// so that we can use "new" command to call method.
+// Example:
+function f1(x, y) {
+    console.log("x=" + x + ",y=" + y);
+}
+new f1(3,4);
+// About argument of function
+// In javascript the number of argument may not equal to the number of formal parameter
+// and the default value of the extra parameter is undefined, but the extra arguments will be ignored。
+// example:
