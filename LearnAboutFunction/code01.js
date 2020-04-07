3183b219337b21c479d2f23f2ceb18543a2fa514
7wc98#14 <2923616405@qq.com>
2020-04-07 20:22:38 +0800

    Update

--- LearnAboutFunction/code01.js
+++ LearnAboutFunction/code01.js
@@ -1,5 +1,17 @@
 // AOP
 // using Function.prototype to achieve AOP
 Function.prototype.before = function (beforeFn) {
-
+    let __self = this;
+    return function () {
+        beforeFn.apply(this, arguments);
+        return __self.apply(this, arguments);
+    }
+};
+Function.prototype.after = function (afterFn) {
+    let __self = this;
+    return function () {
+        let ret = __self.apply(this, arguments);
+        afterFn.apply(this, arguments);
+        return ret;
+    }
 };
